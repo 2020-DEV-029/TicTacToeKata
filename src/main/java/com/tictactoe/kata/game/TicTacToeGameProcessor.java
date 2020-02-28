59919a3dcba45d7bfe0e4dca2a74bd8e250229b2
2020-DEV-029 <ankitksinghal@rediffmail.com>
2020-02-28 21:01:33 +0530

    Committing main game processor class where all the game logic will be implemeted, adding Cross state logic of board

---
+++ src/main/java/com/tictactoe/kata/game/TicTacToeGameProcessor.java
@@ -0,0 +1,76 @@
+package com.tictactoe.kata.game;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Tic Tac Toe game processing class that calculate outcome of game based on
+ * both player choices.
+ * 
+ * @author AKS1405
+ *
+ */
+@Component
+public class TicTacToeGameProcessor {
+
+	private GAME_STATE[][] board = new GAME_STATE[3][3];
+
+	// Initialize board array with blank state of game in constructor
+	public TicTacToeGameProcessor() {
+		for (int row = 0; row < board.length; row++) {
+			for (int col = 0; col < board[row].length; col++) {
+				board[row][col] = GAME_STATE.BLANK;
+			}
+		}
+	}
+
+	/**
+	 * Mark cross in game board and change state of that square to cross and return
+	 * boolean if state changes successfully
+	 * 
+	 * @param row
+	 * @param col
+	 * @return boolean
+	 */
+	public boolean markCross(int row, int col) {
+		return mark(row, col, GAME_STATE.CROSS);
+	}
+
+	/**
+	 * Mark cross in game board and change state of that square to cross and return
+	 * boolean if state changes successfully
+	 * 
+	 * @param row
+	 * @param col
+	 * @param gameState
+	 * @return boolean
+	 */
+	private boolean mark(int row, int col, GAME_STATE gameState) {
+		if (board[row][col] == GAME_STATE.BLANK) {
+			board[row][col] = gameState;
+
+			return true;
+		}
+		return false;
+	}	
+
+	/**
+	 * This method gives current state of board at any time
+	 * 
+	 * @return GAME_STATE[][]
+	 */
+	public GAME_STATE[][] getBoard() {
+		return this.board;
+	}	
+
+	/**
+	 * This method finds whether at any location on board is marked with cross or
+	 * not
+	 * 
+	 * @param row
+	 * @param col
+	 * @return boolean
+	 */
+	public boolean isCross(int row, int col) {
+		return board[row][col] == GAME_STATE.CROSS;
+	}
+}
